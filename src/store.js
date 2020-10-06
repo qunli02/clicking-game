7dd0323f057cbb8ffe81187905f4bc4227bc0c0a
Qunli <qunli02@gmail.com>
2020-10-06 14:36:49 -0400

    must have score to buy

--- src/store.js
+++ src/store.js
@@ -4,8 +4,8 @@ function Store(props) {
   return (
     <div>
       Store
-      <div onClick={() => props.buy(-100, 1)}>1 pointer thing cost 100</div>
-      <div onClick={() => props.buy(-1000, 10)}>10 pointer thing cost 1000</div>
+      <div onClick={() => props.buy(1, -100)}>1 pointer thing cost 100</div>
+      <div onClick={() => props.buy(10, -1000)}>10 pointer thing cost 1000</div>
     </div>
   );
 }
