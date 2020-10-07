9f922f5177c6a176b251fc1d223d57a2e6918b11
Qunli <qunli02@gmail.com>
2020-10-07 15:47:51 -0400

    keeps track of what is brought

---
+++ src/owned.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Owned = (props) => {
+  return <div>lasjdflkjsaldf</div>;
+};
+
+export default Owned;
--- src/store.js
+++ src/store.js
@@ -2,25 +2,34 @@ import React from "react";
 
 function Store(props) {
   return (
-    <div>
+    <div className="store">
       <div>
         points/s Store
-        <button className="store-button" onClick={() => props.buypps(1, -100)}>
+        <button
+          className="store-button"
+          onClick={() => props.buypps(1, -100, "s1")}
+        >
           1 pointer thing cost 100
         </button>
         <button
           className="store-button"
-          onClick={() => props.buypps(10, -1000)}
+          onClick={() => props.buypps(10, -1000, "s2")}
         >
           10 pointer thing cost 1000
         </button>
       </div>
       <div>
         clicker store
-        <button className="store-button" onClick={() => props.buyppc(1, -10)}>
+        <button
+          className="store-button"
+          onClick={() => props.buyppc(1, -10, "c1")}
+        >
           1 extra point pre click cost 10
         </button>
-        <button className="store-button" onClick={() => props.buyppc(10, -100)}>
+        <button
+          className="store-button"
+          onClick={() => props.buyppc(10, -100, "c2")}
+        >
           10 extra point pre click cost 100
         </button>
       </div>
