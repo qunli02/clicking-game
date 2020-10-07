9e1a659f3da3c0f647ad1907f94a263ef1946238
Qunli <qunli02@gmail.com>
2020-10-07 15:54:00 -0400

    fixed bug for tracking brought items

--- src/owned.js
+++ src/owned.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Owned = (props) => {
-  return <div>lasjdflkjsaldf</div>;
+  return (
+    <div>
+      <h1>Owned</h1>
+
+      <p>1 point per click x{props.c1}</p>
+      <p>10 point per click x{props.c2}</p>
+      <p>1 point per sec x{props.s1}</p>
+      <p>10 point per sec x{props.s2}</p>
+    </div>
+  );
 };
 
 export default Owned;
